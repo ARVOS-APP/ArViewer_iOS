137eeec02be5684a95918749b6589fc4fc4f091b
Peter Graf <peter.robert.graf@gmail.com>
2013-03-03 11:37:28 +0100

    Revert "Trying git."
    This reverts commit 232d102d1565fbf88fe9aa04f37df64111ccfbf8.

--- ArViewer-iOS/ARAppDelegate.h
+++ ArViewer-iOS/ARAppDelegate.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2013 Peter Graf. All rights reserved.
 //
 
-/**/
-
 #import <UIKit/UIKit.h>
 
 @interface ARAppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
