593d354cfc54a70f8876e0804954c643d577d59d
Peter Graf <peter.robert.graf@gmail.com>
2013-03-04 01:12:38 +0100

    Added the augment class.

---
+++ ArViewer_iOS/Viewer/ArvosAugment.h
@@ -0,0 +1,38 @@
+/*
+ ArvosAugment.h - ArViewer_iOS
+ 
+ Copyright (C) 2013, Peter Graf
+ 
+ This file is part of Arvos - AR Viewer Open Source for iOS.
+ Arvos is free software.
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ For more information on the AR Viewer Open Source or Peter Graf,
+ please see: http://www.mission-base.com/.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface ArvosAugment : NSObject
+
+@property(nonatomic, strong) NSString* mName;
+@property(nonatomic, strong) NSString* mUrl;
+@property(nonatomic, strong) NSString* mAuthor;
+@property(nonatomic, strong) NSString* mDescription;
+@property(nonatomic) CGFloat mLongitude;
+@property(nonatomic) CGFloat mLatitude;
+@property(nonatomic, strong) NSString* mDeveloperKey;
+
+@end
